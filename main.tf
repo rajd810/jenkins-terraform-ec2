43e7b3eef2e788d63e7c40420d991aa0a26bfe53
Raj Dhumal <rajd810@outlook.com>
2023-07-13 00:12:58 +0530

    changed the resource name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws-instance" "foo" {
+resource "aws_instance" "foo" {
     ami = "ami-437423291903"
     instance_type = "t2.micro"
     tags = {
